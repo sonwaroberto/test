7f2016f2ad3cce567b6b029ebd869c7afee0e6ba
sonwaboris <sonwaboris123@gmail.com>
2022-09-05 07:10:03 +0100

    update

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -27,7 +27,7 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: 'https://cms-bw.herokuapp.com',
+        apiURL: 'https://cms-bw.herokuapp.com/admin',
         accessToken:
           'd9a4558feac4ec632d8c09bd5224350200fef48f571eeee124101bc51e3b6f3f46a5e0920cd6b70ee40396a502585374849fb7365d77d2f1e912c78224f30cd0b7bf482ad1e97364ebc52fe728cf94514354dd5401e4e98f047307bc65f61d0ca24c3246185af952433d93b1eca498591c4a7e3b893b1689b696a2efd21b84c9',
         collectionTypes: ['bw-asset', 'bw-page', 'bw-image'],
